08a27064f489a2bf4a7f7d56a5d3de3c0c485ec1
aembryonic <nico.tama@hotmail.it>
2023-03-10 13:50:29 +0100

    first

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+PACKAGE_VERSION = "0.1"
+MINOR_RELEASE = "0"
+
+setup(
+    name="topic_modeling",
+    version=f"{PACKAGE_VERSION}.{MINOR_RELEASE}",
+    packages=find_packages(where="src"),  # include all packages under src
+    package_dir={"": "src"},
+    include_package_data=True,
+
+    install_requires=[
+        "numpy",
+        "pandas",
+        "bertopic",
+        "umap-learn",
+        "hdbscan",
+        "spacy",
+        "scikit-learn"
+        ],
+
+    author="",
+    author_email="",
+    description="This package contains topic modeling utilities based on BertTopic library",
+    classifiers=[
+        ""
+    ]
+)
